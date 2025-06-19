25dc691c9cb96e6d4d66a2daf19b4ed0ae344e05
tduretz <duretz@uni-frankfurt.de>
2025-06-19 15:27:20 +0200

    test works

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,28 @@ function main()
         tshift = @SVector([-1.0, 0.0])
     
         coeff = bdf_coefficients(tshift)
-        @show coeff
-        # box = BBox(o2, 2, 4)
-        # @test inside(p, box)
+
+        # BDF1 (Backward Euler with Δt = 1.0)
+        coeff_BDF1 = @SVector([1.0, -1.0])
+
+        for i in eachindex(coeff)
+            @test sum(coeff[i] .- coeff_BDF1[i]) ≈ 0
+        end
+    end
+
+
+    @testset "BDF2    " begin
+
+        tshift = @SVector([-2.0, -1.0, 0.0])
+    
+        coeff = bdf_coefficients(tshift)
+
+        # BDF2 (with Δt = 1.0)
+        coeff_BDF2 = @SVector([3/2, -4/3*3/2, 1/3*3/2])
+
+        for i in eachindex(coeff)
+            @test sum(coeff[i] .- coeff_BDF2[i]) ≈ 0
+        end
     end
     
     return
