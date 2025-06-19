ee572d9357c7e22ef801130fd49dc4346fae0d56
tduretz <duretz@uni-frankfurt.de>
2025-06-19 18:45:52 +0200

    add example and readme

---
+++ examples/README.md
@@ -0,0 +1,41 @@
+## BackwardDifferenceFormula.jl
+
+A flexible and efficient tool to evaluate Backward Difference Formula (BDF) coefficients for variable time ste and arbitrary order.
+
+The function `bfd_coefficients()` can be used to determine the BDF coefficient provided the input `tshift` arrays is provided. This array contains the value of relative time from the oldest point to the newest. Since it's a relative values, the newest time has value `0`. `tshift` can be either a `SVector` or a `Tuple`.
+
+### Quick start
+
+#### BDF 1
+
+```julia 
+tshift = @SVector([-1.0, 0.0]) # assumes Δt = 1
+    
+coeff = bdf_coefficients(tshift)
+
+display(coeff)
+```
+The resulting coefficients should be those of backward-Euler, `1/Δt` and `-1/Δt`.
+
+#### BDF 2
+
+```julia 
+tshift = @SVector([-2.0, -1.0, 0.0]) # assumes Δt = 1 and constant
+    
+coeff = bdf_coefficients(tshift)
+
+display(coeff)
+```
+The resulting coefficients should be, for `Δt = 1` , `3/2`, `-2`, `1/2`.
+
+### Example: Visco-Elasto-Plastic rheology integration
+
+This example depicts the integration of visco-elasto-plastic rheology (Maxwell assembly) using various order BDF. This examples uses constant step. The code is provided [here](examples/viscoelastoplastic.jl).
+
+The solution is a stress-time curve which looks like this:
+![](/images/viscoelastoplastic_run.svg).
+
+The theoretical rates of convergence of various order BDF are achieved:
+![](/images/viscoelastoplastic.svg).
+
+
--- examples/viscoelastoplastic.jl
+++ examples/viscoelastoplastic.jl
@@ -2,66 +2,63 @@ using Plots, StaticArrays, BackwardDifferenceFormula, LinearAlgebra
 
 function viscoelastoplastic(bdf, n)
     
-    η      = 10.0
+    # Physical parameters
+    η      = 50.0
     G      = 1.0
     Δt_ref = 1.0/n
     Nt     = 100*n
     ε̇      = 1.0
-    C      = 10
+    C      = 50
 
+    # Initialise solution
     t0    = 0.
     τ     = 2η*ε̇*(1 .- exp.(-G/η.*t0))
     τ0    = 2η*ε̇*(1 .- exp.(-G/η.*(t0-1*Δt_ref)))
     τ00   = 2η*ε̇*(1 .- exp.(-G/η.*(t0-2*Δt_ref)))
     τ000  = 2η*ε̇*(1 .- exp.(-G/η.*(t0-3*Δt_ref)))
     τ0000 = 2η*ε̇*(1 .- exp.(-G/η.*(t0-4*Δt_ref)))
+    τvec  = zeros(Nt)
+    Δt, Δt0, Δt00, Δt000 = Δt_ref, Δt_ref, Δt_ref, Δt_ref
 
-    τvec = zeros(Nt)
-
-    Δt = Δt_ref
-    Δt0, Δt00, Δt000 = Δt, Δt, Δt_ref
+    # Select integrator
+    if bdf==1 
+        tshift = @SVector([-Δt, 0.0])
+        coeff  = bdf_coefficients(tshift)
+        a, b, c, d, e = coeff[1], coeff[2], 0, 0, 0
+    elseif bdf==2 
+        tshift = @SVector([-Δt-Δt0, -Δt, 0.0])
+        coeff  = bdf_coefficients(tshift)
+        a, b, c, d, e = coeff[1], coeff[2], coeff[3], 0, 0
+    elseif bdf==3 
+        tshift = @SVector([-Δt-Δt0-Δt00, -Δt-Δt0, -Δt, 0.0])
+        coeff  = bdf_coefficients(tshift)
+        a, b, c, d, e = coeff[1], coeff[2], coeff[3], coeff[4], 0
+    elseif bdf==4 
+        tshift = @SVector([-Δt-Δt0-Δt00-Δt000, -Δt-Δt0-Δt00, -Δt-Δt0, -Δt, 0.0])
+        coeff  = bdf_coefficients(tshift)
+        a, b, c, d, e = coeff[1], coeff[2], coeff[3], coeff[4], coeff[5]
+    end
 
     # Time stepping
     for it=1:Nt
 
+        # Old stress values
         τ0000 = τ000
         τ000  = τ00
         τ00   = τ0
         τ0    = τ
 
+        # Old time step values
         Δt000 = Δt00
         Δt00  = Δt0
         Δt0   = Δt
-
-        Δt    = Δt_ref
         
-        if bdf==1 
-            tshift = @SVector([-Δt, 0.0])
-            coeff  = bdf_coefficients(tshift)
-            a, b, c, d, e = coeff[1], coeff[2], 0, 0, 0
-        end
-
-        if bdf==2 
-            tshift = @SVector([-Δt-Δt0, -Δt, 0.0])
-            coeff  = bdf_coefficients(tshift)
-            a, b, c, d, e = coeff[1], coeff[2], coeff[3], 0, 0
-        end
-
-        if bdf==3 
-            tshift = @SVector([-Δt-Δt0-Δt00, -Δt-Δt0, -Δt, 0.0])
-            coeff  = bdf_coefficients(tshift)
-            a, b, c, d, e = coeff[1], coeff[2], coeff[3], coeff[4], 0
-        end
-
-        if bdf==4 
-            tshift = @SVector([-Δt-Δt0-Δt00-Δt000, -Δt-Δt0-Δt00, -Δt-Δt0, -Δt, 0.0])
-            coeff  = bdf_coefficients(tshift)
-            a, b, c, d, e = coeff[1], coeff[2], coeff[3], coeff[4], coeff[5]
-        end
-
+        # Visco-elastic trial
         ηve   = ( 1/η + a/G )^-1
         τ     = 2*ηve* (ε̇ - (b*τ0 + c*τ00 + d*τ000 + e*τ0000)/2/G )
         F     = abs(τ) - C
+
+        # Visco-elastic-plastic corrector
         if F>0
             λ̇     = 0.5*F/ηve
             ε̇pl   = λ̇
@@ -70,56 +67,56 @@ function viscoelastoplastic(bdf, n)
         τvec[it] = τ
     end
 
+    # Exact solution
     t   = (1:Nt).*Δt
     τvec_ana = 2η*ε̇*(1 .- exp.(-G/η.*t))
-    τvec_ana[τvec_ana.>C] .= 10
+    τvec_ana[τvec_ana.>C] .= C
     
-    p = plot()
-    p = plot!(t, τvec)
-    p = plot!(t, τvec_ana )
+    # Visualize
+    p = plot(xlabel="Time", ylabel="Stress" )
+    p = plot!(t, τvec, label="Numerics")
+    p = plot!(t, τvec_ana, label="Exact" )
     display(p)
 
-    err = norm(τvec .- τvec_ana)/norm(τvec_ana)
-    return err
-
+    return norm(τvec .- τvec_ana)/norm(τvec_ana)
 end
 
 let
+    # Convergence test
+    res = zeros(4,4)
 
-res = zeros(4,4)
+    @info "BDF1"
+    res[1,1] = viscoelastoplastic(1,1)
+    res[1,2] = viscoelastoplastic(1,2)
+    res[1,3] = viscoelastoplastic(1,4)
+    res[1,4] = viscoelastoplastic(1,8)
+    @show res[1,:]
 
-@info "BDF1"
-res[1,1] = viscoelastoplastic(1,1)
-res[1,2] = viscoelastoplastic(1,2)
-res[1,3] = viscoelastoplastic(1,4)
-res[1,4] = viscoelastoplastic(1,8)
-@show res[1,:]
+    @info "BDF2"
+    res[2,1] = viscoelastoplastic(2,1)
+    res[2,2] = viscoelastoplastic(2,2)
+    res[2,3] = viscoelastoplastic(2,4)
+    res[2,4] = viscoelastoplastic(2,8)
+    @show res[2,:]
 
-@info "BDF2"
-res[2,1] = viscoelastoplastic(2,1)
-res[2,2] = viscoelastoplastic(2,2)
-res[2,3] = viscoelastoplastic(2,4)
-res[2,4] = viscoelastoplastic(2,8)
-@show res[2,:]
+    @info "BDF3"
+    res[3,1] = viscoelastoplastic(3,1)
+    res[3,2] = viscoelastoplastic(3,2)
+    res[3,3] = viscoelastoplastic(3,4)
+    res[3,4] = viscoelastoplastic(3,8)
+    @show res[3,:]
 
-@info "BDF3"
-res[3,1] = viscoelastoplastic(3,1)
-res[3,2] = viscoelastoplastic(3,2)
-res[3,3] = viscoelastoplastic(3,4)
-res[3,4] = viscoelastoplastic(3,8)
-@show res[3,:]
+    @info "BDF4"
+    res[4,1] = viscoelastoplastic(4,1)
+    res[4,2] = viscoelastoplastic(4,2)
+    res[4,3] = viscoelastoplastic(4,4)
+    res[4,4] = viscoelastoplastic(4,8)
+    @show res[4,:]
 
-@info "BDF4"
-res[4,1] = viscoelastoplastic(4,1)
-res[4,2] = viscoelastoplastic(4,2)
-res[4,3] = viscoelastoplastic(4,4)
-res[4,4] = viscoelastoplastic(4,8)
-@show res[4,:]
-
-x = 1 ./[1 2 4 8]
-p=plot(xlabel = "log(1/Δt)", ylabel="Stress error")
-p=plot!(log10.(1 ./x[:]), log10.(res[1,:]), marker=:dot, label="BDF1")
-p=plot!(log10.(1 ./x[:]), log10.(res[2,:]), marker=:dot, label="BDF2")
-p=plot!(log10.(1 ./x[:]), log10.(res[3,:]), marker=:dot, label="BDF3")
-p=plot!(log10.(1 ./x[:]), log10.(res[4,:]), marker=:dot, label="BDF4")
+    x = 1 ./[1 2 4 8]
+    p=plot(xlabel = "log(1/Δt)", ylabel="Stress error", title="Convergence: visco-elasto-plasticity")
+    p=plot!(log10.(1 ./x[:]), log10.(res[1,:]), marker=:dot, label="BDF1")
+    p=plot!(log10.(1 ./x[:]), log10.(res[2,:]), marker=:dot, label="BDF2")
+    p=plot!(log10.(1 ./x[:]), log10.(res[3,:]), marker=:dot, label="BDF3")
+    p=plot!(log10.(1 ./x[:]), log10.(res[4,:]), marker=:dot, label="BDF4")
 end
